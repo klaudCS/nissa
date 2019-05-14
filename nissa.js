ea8a85929347cf647d6fe531ee55d694cfb71a1b
Klavs <klavskruc@gmail.com>
2019-05-14 10:24:54 +0300

    pirmais

---
+++ nissa.js
@@ -0,0 +1,43 @@
+function atzime (atz){
+var atzime = console.readLine();
+if(atzime == 1)
+{
+  console.log(atzime+"🤦🏾‍")
+}
+else if(atzime == 2)
+{
+  console.log(atzime+"👼🏾")
+}
+else if(atzime == 3)
+{
+  console.log(atzime+"😂")
+}
+else if(atzime == 4)
+{
+  console.log(atzime+"👌")
+}
+else if(atzime == 5)
+{
+  console.log(atzime+"🤙🏻")
+}
+else if(atzime == 6)
+{
+  console.log(atzime+"😯")
+}
+else if(atzime == 7)
+{
+  console.log(atzime+"👷🏿‍")
+}
+else if(atzime == 8)
+{
+  console.log(atzime+"🦒")
+}
+else if(atzime == 9)
+{
+  console.log(atzime+"🐷")
+}
+else if(atzime == 10)
+{
+  console.log(atzime+"🐻")
+}
+}
